058870761b451ba5076b928eadc480aa712d4f6b
garri00 <garri1006@gmail.com>
2022-09-03 20:24:08 +0300

    Service structure

---
+++ bin/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":8080", r)
+
+}
